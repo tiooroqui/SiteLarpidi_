1546f9eb39980999a87056a029924767f9003c66
tiooroqui <130712099+tiooroqui@users.noreply.github.com>
2024-12-06 10:04:05 -0300

    script.js

--- SiteLarpidi/script.js
+++ SiteLarpidi/script.js
@@ -1,4 +1,3 @@
-
 document.getElementById("install").addEventListener("click", function () {
-    window.open("https://www.mediafire.com/file/ukgf0x28ir57mxs/application-9d653a4d-f40e-48bf-a323-801454d432ba.apk/file", "_blank");
+    window.open("https://www.mediafire.com/file/ukgf0x28ir57mxs/larpidi.apk/file", "_blank");
 });
